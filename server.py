e92230cec0726ecbf6ae9e1c9af2884480d7608e
Beatriz Pacheco Pinero <bpacheco@gamma06.aulas.gsyc.urjc.es>
2017-11-20 17:01:16 +0100

    Falta revisar el 400 y el 405

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-
+    lista = ['INVITE', 'ACK', 'BYE']
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         self.wfile.write(b"Hemos recibido tu peticion \r\n")
@@ -26,14 +26,22 @@ class EchoHandler(socketserver.DatagramRequestHandler):
                                  b"SIP/2.0 180 Ringing\r\n\r\n"+
                                  b"SIP/2.0 200 OK\r\n\r\n")
             
-            if line.decode('utf-8').split(' ')[0] == 'ACK':
+            elif line.decode('utf-8').split(' ')[0] == 'ACK':
                 #ENVIA LA CANCION
                 pass
             
-            if line.decode('utf-8').split(' ')[0] == 'BYE':
+            elif line.decode('utf-8').split(' ')[0] == 'BYE':
                 self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
 
+            if line.decode('utf-8').split(' ')[0] not in lista:
+            #REVISAR ESTO, NO LO HACE BIEN
+                self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
             
+            else:
+            #REVISAR ESTO, NO LO HACE BIEN. 
+            #CUANDO NO SEA UN 405 Y ESTÉ MAL
+                self.wfile.write(b"SIP/2.0 400 Bad Request\r\n\r\n")
+                   
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
