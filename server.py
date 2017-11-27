b8a15bb3a56a283095d24330afacec317968e0fb
bea <b.pacheco@alumnos.urjc.es>
2017-11-27 20:11:35 +0100

    Arreglados unos fallos a la hora de guardar la ip y puerto y añadidos comentarios de funciones

--- server.py
+++ server.py
@@ -13,59 +13,62 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-    lista = ['INVITE', 'ACK', 'BYE']
+    LISTA = ['INVITE', 'ACK', 'BYE']
 
     def error(self, line):
-        lista_errores = line.split(' ')
+        """
+        method to verify if message is correct
+        """
         fail = False
         try:
-            if len(lista_errores) != 3:
+            if len(line) != 3:
                 fail = True
-            if lista_errores[1][0:4] != 'sip:':
+            if line[1][0:4] != 'sip:':
                 fail = True
-            if '@' not in lista_errores[1]:
+            if '@' not in line[1]:
                 fail = True
-            if ':' not in lista_errores[1]:
+            if ':' not in line[1]:
                 fail = True
-            if 'SIP/2.0\r\n\r\n' not in lista_errores[2]:
+            if 'SIP/2.0\r\n\r\n' not in line[2]:
                 fail = True
         except IndexError:
             fail = True
         return fail
 
     def handle(self):
-        # Escribe dirección y puerto del cliente (de tupla client_address)
+        """
+        handle method of the server class
+        """
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
             print(line.decode('utf-8'))
             if not line:
                 break
-            if self.error(line.decode('utf-8')):
+            list_line_decode = line.decode('utf-8').split(' ')
+            method = list_line_decode[0]
+            if self.error(list_line_decode):
                 self.wfile.write(b"SIP/2.0 400 Bad Request\r\n\r\n")
-            elif line.decode('utf-8').split(' ')[0] == 'INVITE':
+            elif method == 'INVITE':
                 self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n" +
                                  b"SIP/2.0 180 Ringing\r\n\r\n" +
                                  b"SIP/2.0 200 OK\r\n\r\n")
-
-            elif line.decode('utf-8').split(' ')[0] == 'ACK':
+            elif method == 'ACK':
                 aEjecutar = './mp32rtp -i 127.0.0.1 -p 23032 < ' + AUDIO_FILE
                 print('Vamos a ejecutar', aEjecutar)
                 os.system(aEjecutar)
-
-            elif line.decode('utf-8').split(' ')[0] == 'BYE':
+            elif method == 'BYE':
                 self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-
-            elif line.decode('utf-8').split(' ')[0] not in self.lista:
+            elif method not in self.LISTA:
                 self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
 
 
 if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit('Usage: python3 server.py IP port audio_file')
     IP = sys.argv[1]
     PORT = int(sys.argv[2])
     AUDIO_FILE = sys.argv[3]
-    if len(sys.argv) != 4:
-        sys.exit('Usage: python3 server.py IP port audio_file')
     if not os.path.exists(AUDIO_FILE):
         sys.exit("Audio_file doesn't exists")
     # Creamos servidor de eco y escuchamos
